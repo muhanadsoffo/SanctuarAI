5150d7926ef1782767879dff4a68fc7cd5004bc8
muhanadsoffo <muhanadsoffo2002@gmail.com>
2025-06-22 16:12:43 +0300

    adding sliverbar, more work on person detail page

--- lib/views/pages/persons pages/person_details.dart
+++ lib/views/pages/persons pages/person_details.dart
@@ -11,10 +11,12 @@ class PersonDetails extends StatefulWidget {
 }
 
 class _PersonDetailsState extends State<PersonDetails> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Color(0xFF457b9d),
       body: FutureBuilder(
         future: personService.value.getPersonDetails(widget.pid),
         builder: (context, snapshot) {
@@ -25,8 +27,74 @@ class _PersonDetailsState extends State<PersonDetails> {
             return Center(child: Text('there is an error ${snapshot.error}'));
           }
           final person = snapshot.data!;
-          return Column(children: [Center(child: Text(person['name']))]);
+          return CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                expandedHeight: 200,
+                backgroundColor: Color(0xFF457b9d),
+                flexibleSpace: FlexibleSpaceBar(
+
+                  background: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        radius: 70,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    buildButton("Details", 0),
+                    buildButton("Entries", 1),
+                  ],
+                ),
+              ),
+              SliverFillRemaining(
+                hasScrollBody: true,
+                child:  Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(30),
+                    ),
+                  ),
+                  child: IndexedStack(index: selectedIndex,children: [],),
+                ),
+
+              )
+            ],
+          );
         },
+      ),
+    );
+  }
+
+  Widget buildButton(String label, int index) {
+    final bool isSelected = selectedIndex == index;
+    return TextButton(
+      onPressed: () {
+        setState(() => selectedIndex = index);
+      },
+      style: TextButton.styleFrom(
+        backgroundColor: isSelected ? Colors.white : Colors.transparent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        foregroundColor: isSelected ? Colors.green.shade700 : Colors.white,
+        elevation: 0,
+      ),
+      child: Text(
+        label,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
       ),
     );
   }
